ee69ba04bb03345fdc8e8bfb03f617335471b406
MBKCHEL <vasyl.kabanarius@gmail.com>
2026-08-11 14:40:59 +0300

    refactor file (sysinfo)

--- src/sysinfo.rs
+++ src/sysinfo.rs
@@ -1,4 +1,7 @@
 //! Helper functions for retrieving system information.
+
+mod combine;
+
 use std::env;
 use std::fs;
 use std::process::Command;
@@ -45,84 +48,131 @@ impl SystemInfo {
             let _ = set_virtual_terminal(true);
         }
 
-        let mut sys = System::new_all();
-        sys.refresh_all();
-        sys.refresh_cpu_all();
+        let sys = System::new_all();
+
 
         // Collect all system information into a vector of strings
         let mut lines: Vec<String> = Vec::new();
 
+        system_info(&opts, &mut lines);
+
         // --- SYSTEM INFO ---
-        if opts.system {
-            lines.push(format!("{}", "--- System INFO ---".bold().cyan()));
-            lines.push(format!(
-                "{}: {}",
-                "OS".bold(),
-                System::name().unwrap_or_default()
-            ));
-            lines.push(format!(
-                "{}: {}",
-                "OS Version".bold(),
-                System::os_version().unwrap_or_default()
-            ));
-            lines.push(format!(
-                "{}: {}",
-                "Host".bold(),
-                System::host_name().unwrap_or_default()
-            ));
+        fn system_info(opts: &DisplayOptions, lines: &mut Vec<String>){
+
+            if opts.system {
+                lines.push(format!("{}", "--- System INFO ---".bold().cyan()));
+
+
+                os_name(lines);
+                fn os_name(lines: &mut Vec<String>){
+
+                    lines.push(format!(
+                        "{}: {}",
+                        "OS".bold(),
+                        System::name().unwrap_or_default()
+                    ));
+
+                }
+
+                os_version(lines);
+
+                fn os_version(lines: &mut Vec<String>){
+                    lines.push(format!(
+                        "{}: {}",
+                        "OS Version".bold(),
+                        System::os_version().unwrap_or_default()
+                    ));
+                }
+
+                host(lines);
+
+                fn host(lines: &mut Vec<String>){
+                    lines.push(format!(
+                        "{}: {}",
+                        "Host".bold(),
+                        System::host_name().unwrap_or_default()
+                    ));
+                }
+            }
         }
+
+        cpu_info(&opts, &mut lines, &sys);
 
         // --- CPU INFO ---
-        if opts.cpu {
+        fn cpu_info(opts: &DisplayOptions, mut lines: &mut Vec<String>, sys: &System){
+
+            opts.cpu;
             lines.push(format!("{}", "--- CPU INFO ---".bold().cyan()));
 
-            // GHz and name CPU
-            let cpus = sys.cpus();
-            if let Some(cpu) = cpus.first() {
-                lines.push(format!("{}: {}", "CPU".bold(), cpu.brand().trim()));
-                let freq_ghz = cpu.frequency() as f64 / 1000.0;
-                lines.push(format!("{}: {:.2} GHz", "GHz".bold(), freq_ghz));
-            } else {
-                lines.push("CPU: Unknown".to_string());
+            ggz_and_name_cpu(&mut lines, &sys);
+            fn ggz_and_name_cpu(lines: &mut Vec<String>, sys: &System){
+                // GHz and name CPU
+                let cpus = sys.cpus();
+                if let Some(cpu) = cpus.first() {
+                    lines.push(format!("{}: {}", "CPU name".bold(), cpu.brand().trim()));
+                    let freq_ghz = cpu.frequency() as f64 / 1000.0;
+                    lines.push(format!("{}: {:.2} GHz", "GHz".bold(), freq_ghz));
+                } else {
+                    lines.push("CPU: Unknown".to_string());
+                }
             }
 
-            // CPU Usage
-            lines.push(format!(
-                "{}: {:.1}%",
-                "CPU Usage".bold(),
-                sys.global_cpu_usage()
-            ));
+            cpu_usage(&mut lines, &sys);
+
+            fn cpu_usage(lines: &mut Vec<String>, sys: &System){
+                // CPU Usage
+                lines.push(format!(
+                    "{}: {:.1}%",
+                    "CPU Usage".bold(),
+                    sys.global_cpu_usage()
+                ));
+            }
+
+            cpu_temperature(&mut lines);
 
             // CPU Temperature
-            let components = Components::new_with_refreshed_list();
-            let cpu_temp = components.iter().find_map(|comp| {
-                let label = comp.label().to_lowercase();
-                if label.contains("cpu")
-                    || label.contains("core")
-                    || label.contains("package")
-                    || label.contains("k10temp")
-                {
-                    comp.temperature()
-                } else {
-                    None
-                }
-            });
+            fn cpu_temperature(lines: &mut Vec<String>){
 
-            if let Some(temp) = cpu_temp {
-                lines.push(format!("{}: {:.1}°C", "CPU Temp".bold(), temp));
+                let components = Components::new_with_refreshed_list();
+                let cpu_temp = components.iter().find_map(|comp| {
+                    let label = comp.label().to_lowercase();
+                    if label.contains("cpu")
+                        || label.contains("core")
+                        || label.contains("package")
+                        || label.contains("k10temp")
+                    {
+                        comp.temperature()
+                    } else {
+                        None
+                    }
+                });
+
+                if let Some(temp) = cpu_temp {
+                    lines.push(format!("{}: {:.1}°C", "CPU Temp".bold(), temp));
+                }
             }
 
-            // Processor Cores & Threads
-            lines.push(format!(
-                "{}: {}",
-                "Cores".bold(),
-                System::physical_core_count().unwrap_or(0)
-            ));
-            lines.push(format!("{}: {}", "Threads".bold(), cpus.len()));
+            cpu_cores_and_threads(&mut lines, &sys);
+
+            // cpu_cores_and_threads
+            fn cpu_cores_and_threads(lines: &mut Vec<String>, sys: &System) {
+                lines.push(format!(
+                    "{}: {}",
+                    "Cores".bold(),
+                    System::physical_core_count().unwrap_or(0)
+                ));
+                let cpus = sys.cpus();
+                lines.push(format!("{}: {}", "Threads".bold(), cpus.len()));
+            }
+
         }
 
-        // --- MEMORY INFO ---
-        if opts.memory {
+
+        memory_info(&opts, &mut lines, &sys);
+
+        fn memory_info(opts: &DisplayOptions, lines: &mut Vec<String>, sys: &System) {
+            // --- MEMORY INFO ---
+            opts.memory;
             lines.push(format!("{}", "--- Memory INFO ---".bold().cyan()));
             let total_ram = sys.total_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
             let used_ram = sys.used_memory() as f64 / 1024.0 / 1024.0 / 1024.0;
@@ -134,8 +184,11 @@ impl SystemInfo {
             ));
         }
 
-        // --- DISKS INFO ---
-        if opts.disks {
+        disk_info(&opts, &mut lines);
+
+        fn disk_info(opts: &DisplayOptions, lines: &mut Vec<String>) {
+            // --- DISKS INFO ---
+            opts.disks;
             lines.push(format!("{}", "--- Disks INFO ---".bold().cyan()));
             let disks = Disks::new_with_refreshed_list();
             for disk in &disks {
@@ -154,102 +207,124 @@ impl SystemInfo {
             }
         }
 
-        // --- Other Info ---
-        if opts.other {
+
+        // give link functions other_info for variables lines
+        other_info(&opts, &mut lines);
+
+
+        // Functions other_info
+        fn other_info(opts: &DisplayOptions, mut lines: &mut Vec<String>) {
+            // --- Other Info ---
+            opts.other;
             lines.push(format!("{}", "--- Other Info ---".bold().cyan()));
 
-            // Desktop Environment / Window Manager
-            let desktop = env::var("XDG_CURRENT_DESKTOP")
-                .or_else(|_| env::var("DESKTOP_SESSION"))
-                .unwrap_or_else(|_| "Unknown".to_string());
-            lines.push(format!("{}: {}", "DE/WM".bold(), desktop));
+            // give link functions de_wm_check for variables lines
+            de_wm_check(&mut lines);
 
-            let mut battery = "N/A (Desktop)".to_string();
-
-            // Linux battery
-            #[cfg(target_os = "linux")]
-            {
-                if let (Ok(cap), Ok(stat)) = (
-                    fs::read_to_string("/sys/class/power_supply/BAT0/capacity"),
-                    fs::read_to_string("/sys/class/power_supply/BAT0/status"),
-                ) {
-                    battery = format!("{}% [{}]", cap.trim(), stat.trim());
-                } else if let (Ok(cap), Ok(stat)) = (
-                    fs::read_to_string("/sys/class/power_supply/BAT1/capacity"),
-                    fs::read_to_string("/sys/class/power_supply/BAT1/status"),
-                ) {
-                    battery = format!("{}% [{}]", cap.trim(), stat.trim());
-                }
+            // Functions de_wm_check
+            fn de_wm_check(lines: &mut Vec<String>){
+                // Desktop Environment / Window Manager
+                let desktop = env::var("XDG_CURRENT_DESKTOP")
+                    .or_else(|_| env::var("DESKTOP_SESSION"))
+                    .unwrap_or_else(|_| "Unknown".to_string());
+                lines.push(format!("{}: {}", "DE/WM".bold(), desktop));
             }
 
-            // FreeBSD battery
-            #[cfg(target_os = "freebsd")]
-            {
-                let output = Command::new("sysctl")
-                    .arg("-n")
-                    .arg("hw.acpi.battery.life")
-                    .output();
+            // give link functions battery for variables lines
+            battery(&mut lines);
 
-                if let Ok(out) = output {
-                    let cap = String::from_utf8_lossy(&out.stdout).trim().to_string();
-                    if !cap.is_empty() {
-                        battery = format!("{}%", cap);
+            // Functions battery
+            fn battery(lines: &mut Vec<String>){
+                let mut battery = "N/A (Desktop)".to_string();
+
+                // Linux battery
+                #[cfg(target_os = "linux")]
+                {
+
+
+                    if let (Ok(cap), Ok(stat)) = (
+                        fs::read_to_string("/sys/class/power_supply/BAT0/capacity"),
+                        fs::read_to_string("/sys/class/power_supply/BAT0/status"),
+                    ) {
+                        battery = format!("{}% [{}]", cap.trim(), stat.trim());
+                    } else if let (Ok(cap), Ok(stat)) = (
+                        fs::read_to_string("/sys/class/power_supply/BAT1/capacity"),
+                        fs::read_to_string("/sys/class/power_supply/BAT1/status"),
+                    ) {
+                        battery = format!("{}% [{}]", cap.trim(), stat.trim());
                     }
                 }
-            }
-            // OpenBSD battery
-            #[cfg(target_os = "openbsd")]
-            {
-                // apm -l
-                let output = Command::new("apm").arg("-l").output();
 
-                if let Ok(out) = output {
-                    let cap = String::from_utf8_lossy(&out.stdout).trim().to_string();
-                    // 255 в apm означает "батарея не найдена / ПК от сети"
-                    if !cap.is_empty() && cap != "255" {
-                        battery = format!("{}%", cap);
+                // FreeBSD battery
+                #[cfg(target_os = "freebsd")]
+                {
+                    let output = Command::new("sysctl")
+                        .arg("-n")
+                        .arg("hw.acpi.battery.life")
+                        .output();
+
+                    if let Ok(out) = output {
+                        let cap = String::from_utf8_lossy(&out.stdout).trim().to_string();
+                        if !cap.is_empty() {
+                            battery = format!("{}%", cap);
+                        }
                     }
                 }
-            }
-            // NetBSD battery
-            #[cfg(target_os = "netbsd")]
-            {
-                //envstat
-                let output = Command::new("envstat").args(["-s", "bat0:charge"]).output();
+                // OpenBSD battery
+                #[cfg(target_os = "openbsd")]
+                {
+                    // apm -l
+                    let output = Command::new("apm").arg("-l").output();
 
-                if let Ok(out) = output {
-                    let text = String::from_utf8_lossy(&out.stdout);
-                    if let Some(line) = text.lines().find(|l| l.contains("%")) {
-                        if let Some(val) = line.split('(').next() {
-                            let cleaned = val.replace("charge:", "").replace("%", "");
-                            let trimmed = cleaned.trim();
+                    if let Ok(out) = output {
+                        let cap = String::from_utf8_lossy(&out.stdout).trim().to_string();
+                        // 255 в apm означает "батарея не найдена / ПК от сети"
+                        if !cap.is_empty() && cap != "255" {
+                            battery = format!("{}%", cap);
+                        }
+                    }
+                }
+                // NetBSD battery
+                #[cfg(target_os = "netbsd")]
+                {
+                    //envstat
+                    let output = Command::new("envstat").args(["-s", "bat0:charge"]).output();
+
+                    if let Ok(out) = output {
+                        let text = String::from_utf8_lossy(&out.stdout);
+                        if let Some(line) = text.lines().find(|l| l.contains("%")) {
+                            if let Some(val) = line.split('(').next() {
+                                let cleaned = val.replace("charge:", "").replace("%", "");
+                                let trimmed = cleaned.trim();
+                                if !trimmed.is_empty() {
+                                    battery = format!("{}%", trimmed);
+                                }
+                            }
+                        }
+                    }
+                }
+                // Windows battery
+                #[cfg(windows)]
+                {
+                    let output = Command::new("wmic")
+                        .args(["path", "Win32_Battery", "get", "EstimatedChargeRemaining"])
+                        .output();
+
+                    if let Ok(out) = output {
+                        let text = String::from_utf8_lossy(&out.stdout);
+                        if let Some(cap) = text.lines().nth(1) {
+                            let trimmed = cap.trim();
                             if !trimmed.is_empty() {
                                 battery = format!("{}%", trimmed);
                             }
                         }
                     }
                 }
-            }
-            // Windows battery
-            #[cfg(windows)]
-            {
-                let output = Command::new("wmic")
-                    .args(["path", "Win32_Battery", "get", "EstimatedChargeRemaining"])
-                    .output();
 
-                if let Ok(out) = output {
-                    let text = String::from_utf8_lossy(&out.stdout);
-                    if let Some(cap) = text.lines().nth(1) {
-                        let trimmed = cap.trim();
-                        if !trimmed.is_empty() {
-                            battery = format!("{}%", trimmed);
-                        }
-                    }
-                }
+                lines.push(format!("{}: {}", "Battery".bold(), battery));
             }
-
-            lines.push(format!("{}: {}", "Battery".bold(), battery));
         }
+
 
         let gpu = if opts.gpu {
             get_gpu_info()
@@ -270,6 +345,12 @@ impl SystemInfo {
         all
     }
 }
+
+
+
+
+
+
 
 pub fn get_gpu_info() -> Vec<String> {
     let mut gpu_lines = Vec::new();
@@ -312,13 +393,8 @@ pub fn get_gpu_info() -> Vec<String> {
     // Windows GPU
     #[cfg(windows)]
     {
-        let output = Command::new("powershell")
-            .args([
-                "-Command",
-                "Get-CimInstance Win32_VideoController | Select-Object -ExpandProperty Name",
-            ])
+        let output = Command::new("powershell").args(["-Command", "Get-CimInstance Win32_VideoController | Select-Object -ExpandProperty Name", ])
             .output();
-
         if let Ok(out) = output {
             let gpu = String::from_utf8_lossy(&out.stdout).trim().to_string();
             if !gpu.is_empty() {
@@ -419,3 +495,4 @@ pub fn get_gpu_info() -> Vec<String> {
     gpu_lines.push(format!("{}: {}", "GPU".bold(), name));
     gpu_lines
 }
+
