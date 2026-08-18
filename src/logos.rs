c143219f3570d759da6062453864723d2e7eb302
MBKCHEL <vasyl.kabanarius@gmail.com>
2026-08-18 11:19:33 +0300

    fix mini gentoo logo

--- src/logos.rs
+++ src/logos.rs
@@ -1,9 +1,11 @@
+use std::net::ToSocketAddrs;
 use sysinfo::System;
 use colored::*;
 
 pub fn get_logo(mini: bool) -> (Vec<ColoredString>, usize) {
     let os_name = System::name().unwrap_or_default().to_lowercase();
 
+    let os_name = "gentoo".to_string();
     let (raw_logo, color_func): (&str, fn(&str) -> ColoredString) = if mini {
         // Mini
         match os_name.as_str() {
