5d7b5e63fd9a0748d25f538646323b60a8baef5e
MBKCHEL <vasyl.kabanarius@gmail.com>
2026-08-19 17:55:44 +0300

    otimizations

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,12 @@ fn main() {
     sys.refresh_cpu_usage();
 
 
-    thread::sleep(MINIMUM_CPU_UPDATE_INTERVAL);
+
+    use std::time::Instant;
+
+    let start = Instant::now();
+    // твой_модуль::get_info(&opts, &mut lines, &sys);
+    println!("Модуль XXX занял: {:?}", start.elapsed());
 
 
     sys.refresh_cpu_usage();
